73ff60068bd5d02b396eb4dd5a7b371f870ab308
James Anelay <jamesanelay@gmail.com>
2015-06-22 12:20:23 +0100

    Add Setup File

---
+++ app/code/community/TheExtensionLab/MegaMenu/Model/Resource/Setup.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * MegaMenu Setup Resource Model
+ *
+ * @category    TheExtensionLab
+ * @package     TheExtensionLab_MegaMenu
+ * @copyright   Copyright (c) TheExtensionLab (http://www.theextensionlab.com)
+ * @license
+ * @author      James Anelay @ TheExtensionLab <james@theextensionlab.com>
+ */
+class TheExtensionLab_MegaMenu_Model_Resource_Setup
+    extends Mage_Catalog_Model_Resource_Setup
+{
+
+}
---
+++ app/code/community/TheExtensionLab/MegaMenu/sql/theextensionlab_megamenu_setup/install-0.1.0.php
@@ -0,0 +1,8 @@
+<?php
+
+/** @var $installer TheExtensionLab_MegaMenu_Model_Resource_Setup */
+$installer = $this;
+
+$installer->startSetup();
+$installer->installEntities();
+$installer->endSetup();
