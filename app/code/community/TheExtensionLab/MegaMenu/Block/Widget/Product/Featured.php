487aeb973eff9b29d81e764333aefc1b00db2d9c
James Anelay <jamesanelay@gmail.com>
2015-06-27 21:49:31 +0100

    Add featured product block

---
+++ app/code/community/TheExtensionLab/MegaMenu/Block/Widget/Product/Featured.php
@@ -0,0 +1,36 @@
+<?php class TheExtensionLab_MegaMenu_Block_Widget_Product_Featured
+    extends TheExtensionLab_MegaMenu_Block_Widget_Template
+    implements Mage_Widget_Block_Interface
+{
+    protected function getFeaturedProductsCollection()
+    {
+        $featuredProducts = json_decode($this->getMegamenuFeaturedProductIds(), true);
+
+        uasort($featuredProducts, array($this, '_featuredProductsSortByPosition'));
+
+        $featuredProductCollection = $this->_createFeaturedProductsCollectionFromPrefetchedData($featuredProducts);
+
+        return $featuredProductCollection;
+    }
+
+    private function _createFeaturedProductsCollectionFromPrefetchedData(array $featuredProducts)
+    {
+        $featuredProductCollection = new Varien_Data_Collection();
+        $prefetchedCollection = $this->_getPrefetchedProductCollection();
+
+        foreach ($featuredProducts as $featuredProductId => $featuredProduct) {
+            $featuredProductCollection->addItem($prefetchedCollection->getItemById($featuredProductId));
+        }
+
+        return $featuredProductCollection;
+    }
+    private function _getPrefetchedProductCollection()
+    {
+        return Mage::registry('megamenu_products_collection');
+    }
+
+    private static function _featuredProductsSortByPosition($a,$b)
+    {
+        return $a[0]['position'] - $b[0]['position'];
+    }
+}
