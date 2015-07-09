0787240610a3f47e5970f69796b0105c09a621f2
James Anelay <jamesanelay@gmail.com>
2015-07-09 11:25:56 +0100

    Extract methods from Attribute Prefetcher

--- app/code/community/TheExtensionLab/MegaMenu/Model/Prefetcher/Attribute.php
+++ app/code/community/TheExtensionLab/MegaMenu/Model/Prefetcher/Attribute.php
@@ -5,15 +5,27 @@ class TheExtensionLab_MegaMenu_Model_Prefetcher_Attribute
 {
     public function prefetchData(&$prefetchConfig)
     {
+        $collection = $this->getAttributeCollection($prefetchConfig['attribute_ids']);
+        $attributeIdArray = $this->getAttributeIdsArrayFromCollection($collection);
+
+        Mage::register('megamenu_attributes', $attributeIdArray);
+    }
+
+    private function getAttributeIdsArrayFromCollection($collection)
+    {
         $attributeIdArray = array();
-        $collection = Mage::getResourceModel('catalog/product_attribute_collection')
-            ->addFieldToFilter('main_table.attribute_id', array('in' => $prefetchConfig['attribute_ids']))
-            ->addVisibleFilter();
 
         foreach ($collection as $attribute) {
             $attributeIdArray[$attribute->getAttributeId()] = $attribute;
         }
 
-        Mage::register('megamenu_attributes', $attributeIdArray);
+        return $attributeIdArray;
+    }
+
+    private function getAttributeCollection($attributeIds)
+    {
+        return  Mage::getResourceModel('catalog/product_attribute_collection')
+            ->addFieldToFilter('main_table.attribute_id', array('in' => $attributeIds))
+            ->addVisibleFilter();
     }
 }
