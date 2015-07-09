33b734686c8bce7c6def477172401ecde52c675f
James Anelay <jamesanelay@gmail.com>
2015-07-09 17:36:45 +0100

    Fix serializer bugs.
    1) stop wrapping arrays in arrays only do it once
    2) Correct inheritance.

--- js/theextensionlab/megamenu/adminhtml/grid/json/serializer.js
+++ js/theextensionlab/megamenu/adminhtml/grid/json/serializer.js
@@ -4,17 +4,18 @@ String.prototype.addSlashes = function()
 };
 
 var jsonSerializerController = Class.create();
-var clonedSerializer = Object.clone(serializerController.prototype);
-jsonSerializerController.prototype = Object.extend(clonedSerializer, {
+jsonSerializerController.prototype = Object.extend(Object.clone(serializerController.prototype), {
     serializeObject: function(){
         if(this.multidimensionalMode){
             var clone = this.gridData.clone();
             clone.each(function(pair) {
                 //Put the data in array to avoid "}}" which will conflict with the widget directive we now get ]}] which wont match.
                 //Not perfect but better than an unreadable base_64 string which is the default for serializer.
-                var data = [];
-                data.push(pair.value);
-                clone.set(pair.key, data);
+                if(JSON.stringify(pair.value).charAt(0) != "[") {
+                    var data = [];
+                    data.push(pair.value);
+                    clone.set(pair.key, data);
+                }
             });
 
             var jsonString = JSON.stringify(clone.toJSON()).addSlashes();
