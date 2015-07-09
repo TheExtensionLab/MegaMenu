3cbf71d49e54a65de45c21cceed059a479a05693
James Anelay <jamesanelay@gmail.com>
2015-07-09 12:14:37 +0100

    Extract function for Url Rewrite Parser

--- app/code/community/TheExtensionLab/MegaMenu/Model/Parser/Url/Rewrite.php
+++ app/code/community/TheExtensionLab/MegaMenu/Model/Parser/Url/Rewrite.php
@@ -6,13 +6,26 @@ class TheExtensionLab_MegaMenu_Model_Parser_Url_Rewrite
     public function parse($params)
     {
         $prefetchConfig = array();
-        if(isset($params['option_ids']) && isset($params['category_id'])) {
-            $optionIds = json_decode($params['option_ids']);
-            foreach($optionIds as $key => $value):
-                $prefetchConfig['rewrite_ids'][] = 'category/'.$params['category_id'];
-            endforeach;
+        if ($this->_hasValuesRequiredForRewrite()) {
+            $this->_getRewriteIdPathsForOptions($params);
         }
 
         return $prefetchConfig;
+    }
+
+    private function _getRewriteIdPathsForOptions($params)
+    {
+        $optionIds = json_decode($params['option_ids']);
+        $categoryId = $params['category_id'];
+        foreach ($optionIds as $key => $value):
+            $prefetchConfig['rewrite_ids'][] = 'category/' . $categoryId;
+        endforeach;
+
+        return $optionIds;
+    }
+
+    private function _hasValuesRequiredForRewrite()
+    {
+        return isset($params['option_ids']) && isset($params['category_id']);
     }
 }
