ed1cb48548fa2a2e01ee295ccfa80e05eeb60450
James Anelay <jamesanelay@gmail.com>
2015-07-09 12:14:54 +0100

    Extract functions for Featured Product parser

--- app/code/community/TheExtensionLab/MegaMenu/Model/Parser/Product/Featured.php
+++ app/code/community/TheExtensionLab/MegaMenu/Model/Parser/Product/Featured.php
@@ -1,23 +1,27 @@
 <?php
+
 class TheExtensionLab_MegaMenu_Model_Parser_Product_Featured
     implements TheExtensionLab_MegaMenu_Model_Parser_Interface
 {
     public function parse($params)
     {
         $prefetchConfig = array();
-
-        if(isset($params['megamenu_featured_product_ids']))
-        {
-            $featuredProductIds = $params['megamenu_featured_product_ids'];
-
-            $featuredProductsJson = json_decode($featuredProductIds);
-
-            foreach($featuredProductsJson as $featuredProductId => $featuredProductPosition)
-            {
-                $prefetchConfig['product_ids'][] = $featuredProductId;
-            }
+        if ($this->_hasFeaturedProductOption($params)) {
+            $prefetchConfig['product_ids'] = array_keys($this->_getFeaturedProductIdsAsArray($params));
         }
 
         return $prefetchConfig;
+    }
+
+    private function _getFeaturedProductIdsAsArray($params)
+    {
+        $featuredProductIdsJsonString = $params['megamenu_featured_product_ids'];
+        $featuredProductData = json_decode($featuredProductIdsJsonString, 1);
+        return $featuredProductData;
+    }
+
+    private function _hasFeaturedProductOption($params)
+    {
+        return isset($params['megamenu_featured_product_ids']);
     }
 }
