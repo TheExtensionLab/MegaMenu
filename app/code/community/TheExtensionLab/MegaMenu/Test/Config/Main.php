fa20e2a338a8ade070b8119e40252f499e856037
James Anelay <jamesanelay@gmail.com>
2015-07-20 15:48:46 +0100

    Add Featured CMS block widget

--- app/code/community/TheExtensionLab/MegaMenu/Test/Config/Main.php
+++ app/code/community/TheExtensionLab/MegaMenu/Test/Config/Main.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * MegaMenu Main Configuration Tests
  *
@@ -33,4 +34,45 @@ class TheExtensionLab_MegaMenu_Test_Config_Main
         $this->assertSetupResourceExists();
         $this->assertSetupScriptVersions();
     }
+
+    public function testFeaturedCmsWidgetNodeExists()
+    {
+        $widgetConfig = Mage::getModel('widget/widget');
+        $widgetsArray = $widgetConfig->getWidgetsArray();
+
+
+        $this->_assertArrayContainsFeaturedCmsBlock($widgetsArray);
+    }
+
+    private function _assertArrayContainsFeaturedCmsBlock($widgetsArray)
+    {
+        $configContainsCmsFeaturedWidget = false;
+        foreach ($widgetsArray as $widget) {
+            if ($widget['code'] == 'featured_cms_page_menu_block'
+                && $widget['type'] == 'theextensionlab_megamenu/widget_cms_page_featured'
+            ) {
+                $configContainsCmsFeaturedWidget = true;
+                break;
+            }
+        }
+        $this->assertTrue($configContainsCmsFeaturedWidget);
+    }
+
+    public function testFeaturedCmsWidgetContainsParams()
+    {
+        $widgetConfig = Mage::getModel('widget/widget');
+        $widgetXml = $widgetConfig->getWidgetsXml();
+        foreach ($widgetXml as $code => $widget) {
+            if ($code == 'featured_cms_page_menu_block') {
+                $params = $widget->parameters;
+                $reflection = new ReflectionObject($params);
+                $this->assertTrue($reflection->hasProperty('title'));
+                $this->assertTrue($reflection->hasProperty('menu_featured_cms_id'));
+                $this->assertTrue($reflection->hasProperty('display_on'));
+                $this->assertTrue($reflection->hasProperty('template'));
+            }
+        }
+    }
+
+
 }
