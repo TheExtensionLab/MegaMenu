22fae3c1a208561398fa5126232aaac2a6d09ce7
James Anelay <jamesanelay@gmail.com>
2015-07-28 20:46:48 +0100

    revert to function based menu.js

--- skin/frontend/base/default/js/theextensionlab/megamenu/menu.js
+++ skin/frontend/base/default/js/theextensionlab/megamenu/menu.js
@@ -1,3 +1,7 @@
+/*
+ * This file extends from ../menu-original.js which is Copyright (c) 2006-2015 X.commerce, Inc. (http://www.magento.com) under http://opensource.org/licenses/afl-3.0.php Academic Free License (AFL 3.0)
+ */
+
 var bp = {
     xsmall: 479,
     small: 599,
@@ -6,125 +10,133 @@ var bp = {
     xlarge: 1199
 };
 
-var megaMenuManager = new Class.create();
-megaMenuManager.prototype = {
-    initialize : function(navId){
-        this.navId = navId;
-        this.nav = $(this.navId);
-        this.settings = {
+var windowSize = {};
+
+var rtime;
+
+var getWindowSize = function() {
+    windowSize.height = document.viewport.getHeight();
+    windowSize.width = document.viewport.getWidth();
+};
+
+
+Event.observe(window, "delayed:resize", function() {
+    getWindowSize();
+});
+
+var mainNav = function() {
+
+    getWindowSize();
+
+    var main = {
+        obj_nav :   $(arguments[0]) || $("megamenu-nav-main"),
+
+        settings :  {
             show_delay                    :   0,
             hide_delay                    :   0,
             window_resize_delay           :   350,
             timeout                       :   false
-        };
-        this.windowSize = {};
-        this.initWindowSize();
-        this.initMenu();
-    },
+        },
 
-    initWindowSize : function(){
-        this.windowSize.height = document.viewport.getHeight();
-        this.windowSize.width = document.viewport.getWidth();
-    },
+        init :  function(obj, level) {
+            main.initDelayedResizingEvent();
+            obj.lists = obj.childElements();
+            obj.lists.each(function(el,ind){
+                newchildren = el.select('div,ul,section');
+                newchildren.each(function(childel,ind){
+                    main.handlNavElementBubbling(childel);
+                });
+                main.handlNavElement(el);
+            });
+        },
 
-    initMenu : function(){
-        this.initDelayedResizingEvent();
-        this.lists = this.nav.childElements();
-        this.lists.each(this.handleNavElement.bind(this));
-    },
+        initDelayedResizingEvent : function()
+        {
+            Event.observe(window, "resize", function() {
+                rtime = new Date();
+                if (main.settings.timeout === false) {
+                    main.settings.timeout = true;
+                    setTimeout(main.resizeend, main.settings.window_resize_delay);
+                }
+            });
 
-    initDelayedResizingEvent : function(){
-        menu = this;
-        Event.observe(window, "resize", function() {
-            rtime = new Date();
-            if(menu.settings.timeout === false) {
-                menu.settings.timeout = true;
-                setTimeout(this.resizeEnd,menu.settings.window_resize_delay)
+        },
+
+        resizeend : function()
+        {
+            if (new Date() - rtime < main.settings.window_resize_delay) {
+                setTimeout(main.resizeend, main.settings.window_resize_delay);
+            } else {
+                main.settings.timeout = false;
+                Event.fire(window,"delayed:resize");
             }
-        });
-    },
+        },
 
-    resizeEnd : function(){
-        if(new Date() - rtime < this.settings.window_resize_delay) {
-            setTimeout(this.resizeEnd, this.settings.window_resize_delay);
-        } else{
-            this.settings.timeout = false;
-            Event.fire(window,"delayed:resize");
-        }
-    },
+        handlNavElementBubbling :   function(child) {
+            child.onclick = function(e){
+                e.stopPropagation();
+            }
+        },
 
-    handleNavElement : function(list){
-        menu = this;
+        handlNavElement :   function(list) {
+            if(list !== undefined){
 
-        this.preventEventBubbling(list);
+                list.on('touchstart',function(e){
+                    list.writeAttribute('was-touch',true);
+                });
 
-        if(list !== undefined){
-            list.on('touchstart',function(){
-                list.writeAttribute('was-touch',true);
-            });
+                list.onmouseover = function(){
+                    if(windowSize.width < bp.medium && main.obj_nav.hasClassName('responsive')) {
 
-            list.on('mouseover', function(){
-                if(!menu.isSmallScreenAndResponsiveSite(menu)) {
-                    menu.fireNavEvent(this, true);
-                }
-            });
-
-            list.on('mouseout', function(){
-                if(!menu.isSmallScreenAndResponsiveSite(menu)) {
-                    menu.fireNavEvent(this, false);
-                }
-            });
-
-            list.onclick = function(e){
-                if(this.isSmallScreenOrLinkWasTouched(menu,list)) {
-                    if(this.hasClassName('parent') && menu.nav.hasClassName('responsive')) {
-                        e.stop();
-                        menu.fireNavEvent(this, 'toggle');
+                    }else{
+                        main.fireNavEvent(this, true);
                     }
-                }
 
-                menu.cleanUpTouchData(list);
+                };
+                list.onmouseout = function(){
+                    if(windowSize.width < bp.medium && main.obj_nav.hasClassName('responsive')) {
+
+                    }else{
+                        main.fireNavEvent(this, false);
+                    }
+                };
+
+                list.onclick = function(e){
+
+                    if(windowSize.width < bp.medium || list.readAttribute('was-touch')) {
+                        if(this.hasClassName('parent') && main.obj_nav.hasClassName('responsive')) {
+                            e.stop();
+                            main.fireNavEvent(this, 'toggle');
+                        }
+                    }
+
+                    list.writeAttribute('was-touch',false);//cleanup
+                }
+            }
+        },
+
+
+        fireNavEvent :  function(element,active) {
+            if(active == true){
+                element.addClassName("menu-active");
+                element.down("a").addClassName("menu-active");
+            } else if(active == false) {
+                element.removeClassName("menu-active");
+                element.down("a").removeClassName("menu-active");
+            } else if(active == "toggle"){
+                element.toggleClassName("menu-active");
+                element.down("a").toggleClassName("menu-active");
             }
         }
-    },
-
-    isSmallScreenAndResponsiveSite : function(menu){
-        return menu.windowSize.width < bp.medium && menu.nav.hasClassName('responsive');
-    },
-
-    isSmallScreenOrLinkWasTouched : function(menu,list){
-        return menu.windowSize.width < bp.medium || list.readAttribute('was-touch')
-    },
-
-    cleanUpTouchData : function(element){
-        element.writeAttribute('was-touch',false);
-    },
-
-    fireNavEvent : function(element,active){
-        if(active == true){
-            element.addClassName("menu-active");
-            element.down("a").addClassName("menu-active");
-        } else if(active == false) {
-            element.removeClassName("menu-active");
-            element.down("a").removeClassName("menu-active");
-        } else if(active == "toggle"){
-            element.toggleClassName("menu-active");
-            element.down("a").toggleClassName("menu-active");
-        }
-    },
-
-    preventEventBubbling : function(list){
-        newChildren = list.select('div,ul,section');
-        newChildren.each(this.handleNavEventBubbling.bind(this));
-    },
-
-    handleNavEventBubbling : function(child){
-        child.onclick = function(e){
-            e.stopPropagation();
-        }
+    };
+    if (arguments[1]) {
+        main.settings = Object.extend(main.settings, arguments[1]);
+    }
+    if (main.obj_nav) {
+        main.init(main.obj_nav, false);
     }
 };
 
 document.observe("dom:loaded", function() {
-    new megaMenuManager("megamenu-nav-main");
+    mainNav("nav", {"show_delay":"100","hide_delay":"100"});
 });
