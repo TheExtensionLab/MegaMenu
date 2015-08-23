fdd05081d332918c7dd515993b5a8d6d19391f44
James Anelay <jamesanelay@gmail.com>
2015-08-23 23:23:06 +0100

    Add js for dropdown position types

--- skin/frontend/base/default/js/theextensionlab/megamenu/menu.js
+++ skin/frontend/base/default/js/theextensionlab/megamenu/menu.js
@@ -139,4 +139,24 @@ var mainNav = function() {
 
 document.observe("dom:loaded", function() {
     mainNav("nav", {"show_delay":"100","hide_delay":"100"});
+
+    $$('.relative-center').each(function (item, index) {
+        var parentLi = $(item).up('li');
+        var parentLiWidth = parentLi.getWidth();
+        var itemWidth = $(item).getWidth();
+        var requiredRightMargin = ((itemWidth - parentLiWidth) / 2) * -1;
+        $(item).setStyle({
+            'margin-left': requiredRightMargin + 'px'
+        });
+    });
+
+    $$('.hang-left').each(function (item,index) {
+        var parentLi = $(item).up('li');
+        var parentLiWidth = parentLi.getWidth();
+        var itemWidth = $(item).getWidth();
+        var requiredRightMargin = (itemWidth - parentLiWidth) * -1;
+        $(item).setStyle({
+            'margin-left': requiredRightMargin + 'px'
+        });
+    });
 });
