8d301108b0f7a9ea954730a9236f2a41c140410e
James Anelay <jamesanelay@gmail.com>
2015-11-07 21:18:58 +0000

    Add option for per widget category naem override

--- app/code/community/TheExtensionLab/MegaMenu/Block/Widget/Category/List.php
+++ app/code/community/TheExtensionLab/MegaMenu/Block/Widget/Category/List.php
@@ -38,4 +38,13 @@ class TheExtensionLab_MegaMenu_Block_Widget_Category_List
 
         return $childNodes[$categoryNodeId];
     }
+
+    protected function _getCategoryMenuName($categoryNode, $categoryJson)
+    {
+        if(isset($categoryJson->custom_name)){
+            return $categoryJson->custom_name;
+        }
+
+        return $categoryNode->getName();
+    }
 }
